edeeb8bc13d9ffee943370eed18dda96366da4a1
igor <234@gmail.com>
2024-02-26 16:47:02 +0300

    solving task 3

---
+++ task_3.py
@@ -0,0 +1,20 @@
+with open('products.csv', encoding='utf8') as file:
+    # Преобразуем файл в удобный для работы на Python объект
+    data = file.readlines()
+    for i in range(len(data)):
+        data[i] = [obj.strip() for obj in data[i].split(';')]
+
+category = input()
+while category != 'молоко':
+    res_id = -1
+    for i in range(1, len(data)):
+        line = data[i]
+        if line[0] == category:
+            if res_id == -1 or float(line[3]) < float(data[res_id][3]):
+                res_id = i
+
+    if res_id == -1:
+        print('Такой категории не существует в нашей БД')
+    else:
+        print(f'В категории: {data[res_id][0]} товар: {data[res_id][1]} был куплен {data[res_id][-1]} раз')
+    category = input()
