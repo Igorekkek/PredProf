a37a7ad713fc20beb7ea874fb1133831f979edd5
igor <234@gmail.com>
2024-02-26 17:06:34 +0300

    solved task 4

---
+++ task_4.py
@@ -0,0 +1,21 @@
+def gencode(name, date):
+    day, month, year = date.split('.')
+    p1 = name[-2:][::-1]
+    p2 = month[::-1]
+    res = name[:2] + day + p1 + p2
+    return res.upper()
+
+with open('products.csv', encoding='utf8') as file:
+    # Преобразуем файл в удобный для работы на Python объект
+    data = file.readlines()
+    for i in range(len(data)):
+        data[i] = [obj.strip() for obj in data[i].split(';')]
+
+with open('product_promo.csv', encoding='utf8', mode='w') as newfile:
+    data[0].append('promocode')
+    for i in range(1, len(data)):
+        line = data[i]
+        data[i].append(gencode(line[1], line[2]))
+
+    for line in data:
+        newfile.write(';'.join(line) + '\n')
